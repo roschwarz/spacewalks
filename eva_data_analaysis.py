2e8fecfc375f7f89658d9ab76a9872de43cf39ed
roschwarz <schwarzrobert1988@gmail.com>
2025-02-05 14:46:37 +0100

    add comments

--- eva_data_analaysis.py
+++ eva_data_analaysis.py
@@ -2,16 +2,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Data source: https://data.nasa.gov/resource/eva.json (with modifications)
+
 input_file = open('./eva-data.json', 'r')
 output_file = open('./eva-data.csv', 'w')
 graph_file = './cumulative_eva_graph.png'
 
-eva_df = pd.read_json(input_file, convert_dates=['date'])
-eva_df['eva'] = eva_df['eva'].astype(float)
+"""
+load and tidy up data
+"""
+eva_df = pd.read_json(input_file, convert_dates=['date'])  # keep dates as dates
+eva_df['eva'] = eva_df['eva'].astype(float) 
 eva_df.dropna(axis=0, inplace=True)
 eva_df.sort_values('date', inplace=True)
 
 eva_df.to_csv(output_file, index=False)
+
 
 eva_df['duration_hours'] = eva_df['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
 eva_df['cumulative_time'] = eva_df['duration_hours'].cumsum()
