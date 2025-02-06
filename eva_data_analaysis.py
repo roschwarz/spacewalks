2466d74b4f7e64a61bd5ba714ede3405364220b6
roschwarz <schwarzrobert1988@gmail.com>
2025-02-06 10:04:04 +0100

    add order structure

--- eva_data_analaysis.py
+++ eva_data_analaysis.py
@@ -1,6 +1,3 @@
-import json
-import csv
-import datetime as dt
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
@@ -111,13 +108,13 @@ def plot_cumulative_time_in_space(df, graph_file):
 if __name__ == "__main__":
 
     if len(sys.argv) < 3:
-        input_file = './eva-data.json'
-        output_file = './eva-data.csv'
+        input_file = './data/eva-data.json'
+        output_file = './data/eva-data.csv'
         print(f'Using default input and output filenames')
     else:
         input_file = sys.argv[1]
         output_file = sys.argv[2]
         print('Using custom input and output filenames')
 
-    graph_file = './cumulative_eva_graph.png'
+    graph_file = './results/cumulative_eva_graph.png'
     main(input_file, output_file, graph_file)
